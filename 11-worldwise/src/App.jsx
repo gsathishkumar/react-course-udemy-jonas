f97d3169656c97824421ab30a8bf8b0a6ca4928a
Sathishkumar <be.sathish@gmail.com>
2023-09-02 00:40:46 +0530

    Commit after World-wise with Fake Authentication

---
+++ 11-worldwise/src/App.jsx
@@ -0,0 +1,49 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import HomePage from './pages/HomePage';
+import PageNotFound from './pages/PageNotFound';
+import AppLayout from './pages/AppLayout';
+import Login from './pages/Login';
+import CityList from './components/CityList';
+import CountryList from './components/CountryList';
+import City from './components/City';
+import Form from './components/Form';
+import { CitiesProvider } from './contexts/CitiesContext';
+import { AuthProvider } from './contexts/FakeAuthContext';
+import ProtectedRoute from './pages/ProtectedRoute';
+
+function App() {
+  return (
+    <AuthProvider>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<HomePage />}></Route>
+            <Route path="product" element={<Product />}></Route>
+            <Route path="pricing" element={<Pricing />}></Route>
+            <Route path="login" element={<Login />}></Route>
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
+              <Route index element={<Navigate replace to="cities" />} />
+              <Route path="cities" element={<CityList />} />
+              <Route path="cities/:cityId" element={<City />} />
+              <Route path="countries" element={<CountryList />} />
+              <Route path="form" element={<Form />} />
+            </Route>
+            <Route path="*" element={<PageNotFound />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProvider>
+  );
+}
+
+export default App;
---
+++ 11-worldwise/src/contexts/CitiesContext.jsx
@@ -0,0 +1,143 @@
+import { useContext, useReducer, useEffect } from 'react';
+import { createContext } from 'react';
+
+const CitiesContext = createContext();
+const BASE_URL = 'http://localhost:9000';
+
+const initialState = {
+  cities: [],
+  isLoading: false,
+  currentCity: {},
+  error: '',
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case 'loading':
+      return { ...state, isLoading: true };
+    case 'cities/loaded':
+      return { ...state, isLoading: false, cities: action.payload };
+    case 'city/loaded':
+      return {
+        ...state,
+        isLoading: false,
+        currentCity: action.payload,
+      };
+    case 'city/created':
+      return {
+        ...state,
+        isLoading: false,
+        cities: [...state.cities, action.payload],
+        currentCity: action.payload,
+      };
+    case 'city/deleted': {
+      return {
+        ...state,
+        isLoading: false,
+        cities: state.cities.filter(city => city.id !== action.payload),
+        currentCity: {},
+      };
+    }
+    case 'rejected':
+      return { ...state, isLoading: false, error: action.payload };
+    default:
+      throw new Error('Unknown action type');
+  }
+}
+
+function CitiesProvider({ children }) {
+  const [{ cities, isLoading, currentCity, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  useEffect(function () {
+    async function fetchCities() {
+      dispatch({ type: 'loading' });
+      try {
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        dispatch({ type: 'cities/loaded', payload: data });
+      } catch (error) {
+        dispatch({
+          type: 'rejected',
+          payload: 'fetchCities::Error loading cities',
+        });
+      }
+    }
+    fetchCities();
+  }, []);
+
+  async function fetchCityById(cityId) {
+    if (Number(cityId) === currentCity.id) return;
+    dispatch({ type: 'loading' });
+    try {
+      const res = await fetch(`${BASE_URL}/cities/${cityId}`);
+      const data = await res.json();
+      dispatch({ type: 'city/loaded', payload: data });
+    } catch (error) {
+      dispatch({
+        type: 'rejected',
+        payload: 'fetchCityById::Error loading city',
+      });
+    }
+  }
+  async function createCity(newCity) {
+    dispatch({ type: 'loading' });
+    try {
+      const res = await fetch(`${BASE_URL}/cities`, {
+        method: 'POST',
+        body: JSON.stringify(newCity),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const data = await res.json();
+      dispatch({ type: 'city/created', payload: data });
+    } catch (error) {
+      dispatch({
+        type: 'rejected',
+        payload: 'createCity::Error creating city',
+      });
+    }
+  }
+
+  async function deleteCityById(cityId) {
+    dispatch({ type: 'loading' });
+    try {
+      await fetch(`${BASE_URL}/cities/${cityId}`, {
+        method: 'DELETE',
+      });
+      dispatch({ type: 'city/deleted', payload: cityId });
+    } catch (error) {
+      dispatch({
+        type: 'rejected',
+        payload: 'deleteCityById::Error deleting city',
+      });
+    }
+  }
+
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        error,
+        fetchCityById,
+        createCity,
+        deleteCityById,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error('CitiesContext was used outside of CitiesProvider');
+  return context;
+}
+
+export { CitiesProvider, useCities };
